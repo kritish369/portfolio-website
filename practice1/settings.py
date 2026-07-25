50fac22a0c15076465cb16f543e85c9d690736d5
Mac <mac@macbook-pro-m3-420.local>
2026-07-25 18:23:48 +0545

    Fix ALLOWED_HOSTS

--- practice1/settings.py
+++ practice1/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = "django-insecure-i2(&#inb+-k3mp3(%%189#7a)%9p9s^x)8pf+ttz^u+%ykz7zo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
